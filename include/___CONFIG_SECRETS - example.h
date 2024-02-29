75c914beee484bd108f6b73dfacb41148ecdd13a
aly-fly <aljaz@ogrin.me>
2024-02-29 18:53:44 +0100

    fixed hidden secrets file

---
+++ include/___CONFIG_SECRETS - example.h
@@ -0,0 +1,14 @@
+#ifndef __CONFIG_SECRETS_H_
+#define __CONFIG_SECRETS_H_
+
+// ************ WiFi config *********************
+
+#warning Rename this file (remove the " - example" extension) and fill in the data below.
+
+#define WIFI_SSID   "xxxxxx"
+#define WIFI_PASSWD "yyyyyy"
+
+
+
+#endif /* __CONFIG_SECRETS_H_ */
+
